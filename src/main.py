b29d63ba2ad90b3f4a5399f78f29983dff931304
Luvo Hubela (SGT) <luvo.hubela@sanlam.co.za>
2025-06-05 10:26:47 +0200

    updated

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     data = [
-        {"cluster": "Sanlam Life Savings (SLS)", "business unit": "Sanlam Retail Mass", "department": "Assupol"},
-        {"cluster": "Sanlam Emerging Markets (SEM)", "business unit": "Sanlam Pan-Africa (SPA)", "department": "Life Insurance"},
-        {"cluster": "Sanlam Investment", "business unit": "Asset Management", "department": "Satrix"},
-        {"cluster": "Santam", "business unit": "Santam Client Solutions", "department": "Client Service Operations"},
-        {"cluster": "Sanlam Fintech", "business unit": "Sanlam Rewards", "department": "Sanlam Reality Programme"},
+        {"cluster": "Sanlam Life Savings (SLS)", "businessunit": "Sanlam Retail Mass", "department": "Assupol"},
+        {"cluster": "Sanlam Emerging Markets (SEM)", "businessunit": "Sanlam Pan-Africa (SPA)", "department": "Life Insurance"},
+        {"cluster": "Sanlam Investment", "businessunit": "Asset Management", "department": "Satrix"},
+        {"cluster": "Santam", "businessunit": "Santam Client Solutions", "department": "Client Service Operations"},
+        {"cluster": "Sanlam Fintech", "businessunit": "Sanlam Rewards", "department": "Sanlam Reality Programme"},
         ]
     return render_template("index.html", data=data)
 
